59e10ffd73a3744d5b4f807b0c6595a52ab668ab
ritika <ritikasah237@gmail.com>
2025-03-29 10:33:45 +0530

    Changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ function Nav() {
     <>
      <div className="navbar bg-base-100 shadow-sm">
   <div className="flex-1">
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    <a className="btn btn-ghost text-xl">Travel Vista</a>
   </div>
   <div className="flex-none">
     <div className="dropdown dropdown-end">
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -20,12 +20,12 @@ function Home() {
       src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
       className="max-w-sm rounded-lg shadow-2xl" />
     <div>
-      <h1 className="text-5xl font-bold">Box Office News!</h1>
+      <h1 className="text-5xl font-bold">Plan Your Next Beautiful Adventure. </h1>
       <p className="py-6">
         Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-        quasi. In deleniti eaque aut repudiandae et a id nisi.
+        quasi. In deleniti eaque aut repudiandae et a id nisi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum quidem consectetur tempora dignissimos quod pariatur, accusantium nam magni, perspiciatis officiis et unde nostrum ipsum hic expedita suscipit. Sed repellendus itaque ipsa mollitia impedit esse.
       </p>
-      <button className="btn btn-primary">Get Started</button>
+      <button className="btn bg-amber-700">Explore More!</button>
     </div>
   </div>
 </div>
